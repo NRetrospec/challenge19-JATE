3e69a076982548cee69d7f5b5b531edde7e95b5b
Phresh . <marquisej.galo@gmail.com>
2025-02-13 06:53:07 -0500

    final READ ME

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# challenge19-JATE
+# JATE
+
+## Description
+JATE (Just Another Text Editor) is a web-based text editor that allows users to create and edit documents with a simple and intuitive interface. The application supports offline functionality and is designed to be a Progressive Web App (PWA).
+
+## Installation
+To install the application, clone the repository and run the following commands:
+
+```bash
+# Install server dependencies
+cd server
+npm install
+
+# Install client dependencies
+cd ../client
+npm install
+```
+
+## Usage
+To start the application in development mode, run:
+
+```bash
+npm run start:dev
+```
+
+This command will start both the server and client concurrently.
+
+To build the client for production, run:
+
+```bash
+npm run build
+```
+
+Then, start the server:
+
+```bash
+npm start
+```
+
+## Contributing
+Contributions are welcome! Please submit a pull request or open an issue for any enhancements or bug fixes.
+
+## License
+This project is licensed under the MIT License.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,39 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
+      new WebpackPwaManifest({
+        name: 'J.A.T.E',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+        start_url: '.',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('icons'),
+            ios: true,
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
